c60321f84f73a0122a820a211223b85b027a67af
andye2801 <andy.elliott90@gmail.com>
2025-01-10 13:53:54 +0000

    Update google-doc-url.js
    Add API key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1QBOgTaAGqglMlPlRxJCKdL2oYbJ7JkYrJyQS367Nx0w/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBjuddqERq49Op_W3eYi1ZWQD6CBYdlgQg';
